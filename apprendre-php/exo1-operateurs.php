0cecbcf76bee8cf425b506e0716c93696fafe67a
Kellis <vikiclashonyt@gmail.com>
2021-01-20 17:11:19 +0100

    Add files via upload

---
+++ apprendre-php/exo1-operateurs.php
@@ -0,0 +1,26 @@
+﻿<?php
+
+/*==================================================*/
+// Exercice 1 - les opérateurs
+// Créer un code PHP permettant de calculer le prix total HT et le prix total TTC pour l'achat de x articles et les afficher.
+// Affecter les valeurs 0.206 pour le taux de TVA, 100 pour le prix du produit et 10 pour les quantités
+// On affichera dans un premier temps le prix HT du produit, le taux de TVA en % ainsi que la quantité commandée puis le résultat HT et TTC.
+/*==================================================*/
+
+$tva = 0.206;
+$price = 100;
+$quant = 10;
+
+printf("HT (unité): %s€<br>
+        TVA (%%): %s%%<br>
+        Quantité: %s<br>
+        HT (total): %s€<br>
+        TTC (total): %s€<br>", 
+        $price, $tva*100, $quant, $price*$quant, (($price*$quant)*$tva)+($price*$quant)
+);
+$age = 22;
+if ($age >= 18) {
+    echo "tu es majeur";
+}
+?>
+
---
+++ apprendre-php/exo2-conditions.php
@@ -0,0 +1,15 @@
+﻿<?php
+
+/*==================================================*/
+// Exercice 2 - les conditions
+// Créer un code PHP permettant d'afficher un message différent en fonction des conditions (if else).
+// Le script devra afficher un message de bienvenue uniquement si l'utilisateur est majeur et parle Français ou Anglais.
+// Un message type sera affiché en cas de non respect de ces 2 conditions...
+/*==================================================*/
+
+$age = 19;
+$language = 'en';
+
+echo($age >= 18 ? ($language == 'fr' || $language == 'en' ? ($language == 'fr' ? "Bienvenue" : "Welcome") : "Tu n'es ni français ni anglais") : "Tu n'as pas l'âge requis");
+
+?>
